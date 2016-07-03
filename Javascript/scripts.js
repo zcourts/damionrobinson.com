c868c14702e82f10bff021c60101c55009f217be
Damion.Graphics <damion.graphics@gmail.com>
2016-07-03 21:36:15 +0100

    modification to the entire site

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -1,28 +1,33 @@
 // Empty JS for your own code to be here
 
 $(document).ready(function () {
-	// Add smooth scrolling to all links
-	$("a").on('click', function (event) {
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
 
-		// Make sure this.hash has a value before overriding default behavior
-		if (this.hash !== "") {
-			// Prevent default anchor click behavior
-			event.preventDefault();
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
 
-			// Store hash
-			var hash = this.hash;
+            // Store hash
+            var hash = this.hash;
 
-			// Using jQuery's animate() method to add smooth page scroll
-			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-			$('html, body').animate({
-				scrollTop: $(hash).offset().top
-			}, 900, function () {
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function () {
 
-				// Add hash (#) to URL when done scrolling (default click behavior)
-				window.location.hash = hash;
-			});
-		} // End if
-	});
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
 });
 
 
+// Lazy load
+
+$("img.lazy").lazyload({
+    effect: "fadeIn"
+});
