539950b99c041f462c4be4487b489b18fcea09f1
Damion.Graphics <damion.graphics@gmail.com>
2016-07-04 19:57:59 +0100

    lazy working model

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -26,12 +26,55 @@ $(document).ready(function () {
 });
 
 
+//Home page transition
+//slider images
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1
+    }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 10000); // Change image every 10 seconds
+}
+
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-red", "");
+    }
+    x[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " w3-red";
+}
+
+// End Home Page transition
+
+
 // Lazy load
-
-$(document).ready(function () {
-
-    $("img.lazy").lazyload({
-        effect : "fadeIn"
-    });
-
+$("img.lazy").lazyload({
+    effect : "fadeIn"
 });
+   
